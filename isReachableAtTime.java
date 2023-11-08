9d6f722d5850abf8f043f2d8de6e40bbd8bfad85
Kevin Thomas Manjooran <kevinthomastm2002@gmail.com>
2023-11-08 08:36:58 +0530

    Create isReachableAtTime.java

---
+++ isReachableAtTime.java
@@ -0,0 +1,10 @@
+class isReachableAtTime {
+  public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+    final int minStep = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
+    if (minStep == 0) {
+      return t != 1;
+    }
+    
+    return minStep <= t;
+  }
+}
